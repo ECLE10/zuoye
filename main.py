482937545b5b2373fd33422c00563590a0dd0bee
ECLE10 <71075878+ECLE10@users.noreply.github.com>
2020-09-25 10:36:04 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,148 +1,73 @@
-#-*-coding : utf-8 -*-
-from functools import reduce
 import jieba
-import jieba.analyse
-import math
+import re
 import sys
+from jieba import analyse
+from gensim import corpora, models, similarities
 
-
-def resemble_cal(all_key, article1_dic, article2_dic):
-    str1_vector = []
-    str2_vector = []
-    # 计算词频向量
-    for i in all_key:
-        # 返回字典article1_dic中i元素对应的值,若无，则进行初始化
-        # 若不存在article1_dic，则字典article1_dic中生成i元素，并使其对应的数字为0
-        str1_count = article1_dic.get(i, 0)
-        # append函数会在数组后加上相应的元素
-        str1_vector.append(str1_count)
-        str2_count = article2_dic.get(i, 0)
-        str2_vector.append(str2_count)
-    # 计算各自平方和
-    # map() 会根据提供的函数对指定序列做映射
-    str1_map = map(lambda x: x * x, str1_vector)
-    str2_map = map(lambda x: x * x, str2_vector)
-    # reduce() 函数会对参数序列中元素进行累积
-    str1_mod = reduce(lambda x, y: x + y, str1_map)
-    str2_mod = reduce(lambda x, y: x + y, str2_map)
-    # 计算平方根
-    str1_mod = math.sqrt(str1_mod)
-    str2_mod = math.sqrt(str2_mod)
-    # 计算向量积
-    vector_multi = reduce(lambda x, y: x + y,
-                          map(lambda x, y: x * y, str1_vector, str2_vector))
-    # 计算余弦值
-    cos = float(vector_multi) / (str1_mod * str2_mod)
-    return cos
-    return return_dic
-
-
-'''
-文章关键词提取
-'''
-
-
-def analyse_word(content):
-    # 新建两个数组
-    zidian = {}
-    return_dic = {}
-    # 内容分词
-    # jieba.cut_for_search 方法接受两个参数：需要分词的字符串；是否使用 HMM 模型。
-    # 该方法适合用于搜索引擎构建倒排索引的分词，粒度比较细
-    fenci = jieba.cut_for_search(content)
-    for fc in fenci:
-        # 如果fc在fenci中
-        if fc in zidian:
-            zidian[fc] += 1
+class NoWord(Exception):
+    def __init__(self):
+        print("这个文件是空文本")      
+# 计算文章的相似度
+def Split_sentence(file_txt):
+    head = '\u4e00'
+    tail = '\u9fa5'
+    word = ""
+    sentence_list = []  # 把拆分的句子保存下来
+    for each in range(len(file_txt)):
+        if head <= file_txt[each] <= tail:  # 中文编码范围
+            word += file_txt[each]
+        elif file_txt[each] == "，":  # 用逗号来分句
+            sentence_list.append(word)
+            word = ""
         else:
-            zidian[fc] = 1
-    # jieba.analyse.extract_tags(sentence, topK=5, withWeight=True, allowPOS=())
-    # content 需要提取的字符串，必须是str类型，不能是list
-    # topK 提取前多少个关键字
-    # withWeight 是否返回每个关键词的权重
-    # allowPOS是允许的提取的词性，默认为allowPOS=‘ns’, ‘n’, ‘vn’, ‘v’，提取地名、名词、动名词、动词
-
-    # 关键字的个数
-    topK = 100
-    tfidf = jieba.analyse.extract_tags(content, topK=topK, withWeight=True)
-    try:
-        artice = open(
-            'C:/Users/Administrator.USER-20190905VU/Desktop/3118005403/requirements/stop_word.txt',
-            'r',
-            encoding='utf-8').read()
-    except FileNotFoundError:
-        msg = "请输入正确的文件路径"
-        print(msg)
-    # 要转换成字符串
-    # strip() 方法用于移除字符串头尾指定的字符（默认为空格或换行符）或字符序列
-    # 移除停用库里面的词
-    stopkeyword = [content.strip(str(artice))]
-    # 在tfidf中的字符中2
-    for word_weight in tfidf:
-        # 如果这个在停用词中
-        if word_weight in stopkeyword:
-            # continue 语句跳出本次循环，而break跳出整个循环。
-            # continue 语句是一个删除的效果，他的存在是为了删除满足循环条件下的某些不需要的成分
             continue
-        frequence = zidian.get(word_weight[0], 'not found')
-        return_dic[word_weight[0]] = frequence
-    return return_dic
+    if word != '':
+        sentence_list.append(word)
+        word = ''
 
-# 保留两位小数
-def result(num):
-    num_str = str(num).split('.')
-    num = float(num_str[0] + '.' + num_str[1][0:2])
-    return num
+    return sentence_list
+def Calculation_Similiarity(origin_txt,origin_add_txt):
+    sim_value = []
+    word_lenth = []
+    total_sum = 0
+    total_size = 0
+    # 利用jieba.luct进行分词，并将其保存在list列表中
+    ori_list = [[word for word in jieba.lcut(sentence)] for sentence in origin_txt]
+    ori_add_list = [[word for word in jieba.lcut(sentence)] for sentence in origin_add_txt]
+    # 生成词典
+    dictionary = corpora.Dictionary(ori_list)
+    # 通过doc2bow稀疏向量生成语料库
+    corpus = [dictionary.doc2bow(word) for word in ori_list]
+    # 通过TF模型算法，计算出tf值
+    tf = models.TfidfModel(corpus)
+    # 通过token2id得到特征数（字典里面键的个数）
+    num_features = len(dictionary.token2id.keys())
+    # 计算稀疏矩阵相似度，并建立索引
+    index = similarities.MatrixSimilarity(tf[corpus], num_features=num_features)
+    # 每句长度-单个变量
+    word_size = 0
+    size = 0
+    sim = 0
+    for word in range(len(ori_add_list)):
+        # 新的稀疏向量
+        new_vector = dictionary.doc2bow(ori_add_list[word])
+        # 算出文章的相似度
+        sim_list = index[tf[new_vector]]
+        # 选出最大相似度
+        sim = max(sim_list)
+        # 加入相似度列表
+        sim_value.append(sim)
+        # 相似文章每句长度值
+        word_size = len(ori_add_list[word])
+        # 文章总长度值
+        size += word_size
+        # 加入长度列表
+        word_lenth.append(word_size)
+    total_size = size
+    for i in range(len(word_lenth)):
+        total_sum += word_lenth[i] * sim_value[i]
 
-def main():
-    # set是把重复的去掉
-    all_key = set()
-    # 打开文件
-    try:
-        f = open(
-            sys.argv[1],
-            'r',
-            encoding='utf-8')
-        lines = f.readlines()
-        # join()是一个字符串方法，它返回被子字符串连接的字符串。
-        lines = "".join(lines)
-        # print(lines)
-        # 得到关键字典
-        article1_dic = analyse_word(lines)
-    except FileNotFoundError:
-        msg = "请输入正确的文件路径"
-        print(msg)
-    print('第一篇文章的关键字和词频', article1_dic)
-    # 如果kv在字典中则添加到all_key中
-    # for循环中k和v都是变量，分别遍历了key，value
-    for k, v in article1_dic.items():
-        all_key.add(k)
-    try:
-        g = open(
-            sys.argv[2],
-            'r',
-            encoding='utf-8')
-        lines1 = g.readlines()
-        lines1 = "".join(lines1)
-        # print(lines1)
-        article2_dic = analyse_word(lines1)
-    except FileNotFoundError:
-        msg = "请输入正确的文件路径"
-        print(msg)
-    print('第二篇文章的关键字和词频', article2_dic)
-    for k, v in article2_dic.items():
-        all_key.add(k)
-        # 进行计算
-    cos = resemble_cal(all_key, article1_dic, article2_dic)
-    cosfinal = result(cos)
-    # output = input('请输入答案文件的地址：')
-    with open(sys.argv[3], 'w') as file:
-        file.write(str(cosfinal))
-    file.close()
-    print('两篇文章的相似度:', cosfinal)
-    
-    
+    # 加权求平均
+    ans = total_sum / total_size
 
-
-if __name__ == "__main__":
-    main()
+    return ans
